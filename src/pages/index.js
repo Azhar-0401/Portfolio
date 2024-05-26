e4832875a34b40278819f212c97e5990968ff45d
Azhar-0401 <azharansari.surajpur@gmail.com>
2024-05-26 20:53:49 +0530

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
-  About, Blog, Contacts, Education,
+  About,  Contacts, Education,
   Experience, Landing, Navbar, Projects, Skills
 } from '../components';
 import BackToTop from '../components/back-to-top/back-to-top';
 import ChangeTheme from '../components/change-theme/change-theme';
 
-function HomePage({ blogs }) {
+function HomePage() {
 
   return (
     <>
@@ -25,16 +25,16 @@ function HomePage({ blogs }) {
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch('https://dev.to/api/articles?username=said7388');
-  const data = await res.json();
-  const filteredBlogs = data.sort(() => Math.random() - 0.5);
+// export async function getStaticProps() {
+//   const res = await fetch('https://dev.to/api/articles?username=said7388');
+//   const data = await res.json();
+//   const filteredBlogs = data.sort(() => Math.random() - 0.5);
 
-  return {
-    props: {
-      blogs: filteredBlogs
-    },
-  };
-}
+//   return {
+//     props: {
+//       blogs: filteredBlogs
+//     },
+//   };
+// }
 
 export default HomePage
